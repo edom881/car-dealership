ecf3e25dadfc4eb1638bc00ba3cbedb05ea23646
edomg <edomgezahegn54@gmail.com>
2026-05-12 13:02:52 -0500

    Add Dealership constructor

--- src/main/java/com/pluralsight/Dealership.java
+++ src/main/java/com/pluralsight/Dealership.java
@@ -10,5 +10,11 @@ public class Dealership {
 
     private ArrayList<Vehicle> inventory;
 
+    public Dealership(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.inventory = new ArrayList<>();
+    }
 }
 
