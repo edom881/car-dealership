94abfdd6c89f2ad486e705d7e882ca18a31da688
edomg <edomgezahegn54@gmail.com>
2026-05-12 15:44:29 -0500

    Complete UserInterface menu and helper methods

--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -3,10 +3,58 @@ package com.pluralsight;
 import java.util.Scanner;
 
 public class UserInterface {
-
     private Dealership dealership;
     private Scanner scanner = new Scanner(System.in);
+
     public void display() {
 
+        init();
+
+        boolean running = true;
+
+        while (running) {
+
+            System.out.println("\n--- Car Dealership Menu ---");
+            System.out.println("1 - List all vehicles");
+            System.out.println("99 - Quit");
+
+            System.out.print("Enter your choice: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice) {
+
+                case 1:
+                    processAllVehiclesRequest();
+                    break;
+
+                case 99:
+                    running = false;
+                    System.out.println("Goodbye!");
+                    break;
+
+                default:
+                    System.out.println("Invalid choice.");
+            }
+        }
     }
+
+    private void init() {
+
+        DealershipFileManager fileManager = new DealershipFileManager();
+        dealership = fileManager.getDealership();
+    }
+
+    private void processAllVehiclesRequest() {
+
+        displayVehicles(dealership.getAllVehicles());
+    }
+
+    private void displayVehicles(java.util.ArrayList<Vehicle> vehicles) {
+
+        for (Vehicle vehicle : vehicles) {
+            System.out.println(vehicle);
+        }
+    }
+
 }
