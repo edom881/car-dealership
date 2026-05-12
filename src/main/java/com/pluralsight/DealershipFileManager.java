e1116dc289cd07fb3711b9bb71f9fb7e6c830177
edomg <edomgezahegn54@gmail.com>
2026-05-12 14:09:25 -0500

    Create Dealership object from file data

--- src/main/java/com/pluralsight/DealershipFileManager.java
+++ src/main/java/com/pluralsight/DealershipFileManager.java
@@ -13,6 +13,12 @@ public class DealershipFileManager {
         try { BufferedReader reader = new BufferedReader(new FileReader(FILE_NAME));
             String dealershipLine = reader.readLine();
             String[] dealershipParts = dealershipLine.split("\\|");
+            Dealership dealership = new Dealership(
+                    dealershipParts[0],
+                    dealershipParts[1],
+                    dealershipParts[2]
+            );
+
 
         } catch (IOException e) {
             System.out.println("Error reading inventory file.");
