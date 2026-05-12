424528629bab84eea1bcb8320d80f9bea36b4dbf
edomg <edomgezahegn54@gmail.com>
2026-05-12 13:15:24 -0500

    Add inventory file setup

--- src/main/java/com/pluralsight/DealershipFileManager.java
+++ src/main/java/com/pluralsight/DealershipFileManager.java
@@ -1,4 +1,11 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class DealershipFileManager {
+
+    private static final String FILE_NAME = "inventory.csv";
+
 }
